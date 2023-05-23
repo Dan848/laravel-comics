85df869c1107bc337264d67ff69cfefb77e3ea80
Dan848 <daniele.ceravolo15@gmail.com>
2023-05-23 05:55:01 +0200

    add first section comic page

--- resources/views/comics/comic.blade.php
+++ resources/views/comics/comic.blade.php
@@ -13,14 +13,31 @@
             </div>
         </div>
     </div>
-    <div class="p-5 container">
+    <div class="container p-5">
         <div class="row">
-            <div class="col-8">
-                <h2>{{$comic["title"]}}</h2>
+            <div class="col">
+                <h2 class="mb-4">{{$comic["title"]}}</h2>
+                <div class="container price-checker py-3 mt-1 my-3">
+                    <div class="row">
+                        <div class="col d-flex justify-content-between fw-bold px-4">
+                            <div>
+                                <span class="opacity-75">U.S. Price:</span>
+                                {{$comic["price"]}}
+                            </div>
+                           <div class="text-uppercase">available</div>
+                        </div>
+                        <div class="col-auto px-4 border-start">
+                            Check Availability
+                        </div>
+                    </div>
+                </div>
+                <p class="">
+                    {{$comic["description"]}}
+                </p>
             </div>
-            <div class="col-4">
+            <div class="col-auto text-end">
                 <h6 class="text-uppercase text-end">Advertisement</h6>
-                <img src="/img/adv.jpg" alt="">
+                <img src="/img/publ.jpg" alt="">
             </div>
         </div>
     </div>
